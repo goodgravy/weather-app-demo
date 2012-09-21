1e3c1b4d7214a89a1bc1b69393c68d4408b61164
Antoine van Gelder <antoine@trigger.io>
2012-09-21 13:32:16 +0200

    Part 2

--- js/weather.js
+++ js/weather.js
@@ -39,4 +39,32 @@ var weather = {
 	}
 };
 
-forge.logging.info(JSON.stringify(weather));
+
+function populateWeatherConditions (weather) {
+	var tmpl, output;
+
+	forge.logging.log("[populateWeatherConditions] beginning populating weather conditions");
+
+	tmpl = $("#forecast_information_tmpl").html();
+	output = Mustache.to_html(tmpl, weather.current_observation);
+	$("#forecast_information").append(output);
+	forge.logging.log("[populateWeatherConditions] finished populating forecast information");
+
+	tmpl = $("#current_conditions_tmpl").html();
+	output = Mustache.to_html(tmpl, weather.current_observation);
+	$("#current_conditions").append(output);
+	forge.logging.log("[populateWeatherConditions] finished populating current conditions");
+
+	tmpl = $("#forecast_conditions_tmpl").html();
+	output = Mustache.to_html(tmpl, weather.forecast.simpleforecast);
+	$("#forecast_conditions table tr").append(output);
+	forge.logging.log("[populateWeatherConditions] finished populating forecast conditions");
+
+	forge.logging.log("[populateWeatherConditions] finished populating weather conditions");
+};
+
+
+$(function () {
+	populateWeatherConditions(weather);
+});
+
