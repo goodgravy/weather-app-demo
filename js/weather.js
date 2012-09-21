2f0ab81ca1d87c13b864fa87f4ca2c3d29dbf00f
Antoine van Gelder <antoine@trigger.io>
2012-09-21 13:32:48 +0200

    Part 4

--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,8 @@
 function populateWeatherConditions (weather) {
 	var tmpl, output;
 
+	emptyContent(); 
+
 	forge.logging.log("[populateWeatherConditions] beginning populating weather conditions");
 
 	tmpl = $("#forecast_information_tmpl").html();
@@ -40,7 +42,52 @@ function getWeatherInfo(location, callback) {
 };
 
 
+function emptyContent() {
+	forge.logging.log("[emptyContent] removing old data");
+	$("#forecast_information").empty();
+	$("#current_conditions").empty();
+	$("#forecast_conditions table tr").empty();
+	
+	forge.logging.log("[emptyContent] finished emptying content");
+};
+
+
 $(function () {
-	getWeatherInfo("CA/San_Francisco", populateWeatherConditions);
+	var cities = [ 
+		{ name: "London", code: "UK/London" },
+		{ name: "San Francisco", code: "CA/San_Francisco" },
+		{ name: "Cape Town", code: "ZA/Cape_Town" },
+		{ name: "Barcelona", code: "ES/Barcelona" },
+		{ name: "Boston", code: "NY/Boston" },
+		{ name: "New York", code: "NY/New_York" },
+		{ name: "Washington DC", code: "DC/Washington" },
+		{ name: "Tampa", code: "FL/Tampa" },
+		{ name: "Houston", code: "AL/Houston" },
+		{ name: "Montreal", code: "CYUL" },
+		{ name: "Los Angeles", code: "CA/Los_Angeles" },
+		{ name: "Miami", code: "FL/Miami" },
+		{ name: "West Palm Beach", code: "FL/West_Palm_Beach" } 
+	];
+	cities.forEach(function(city) {	
+		$("#city_menu").append("<option value='" + city.code + "'>" + city.name + "</option>");
+	});
+	$("#city_menu").change(function() {
+		var city = $("#city_menu option:selected").val();
+		forge.prefs.set("city", city);
+		getWeatherInfo(city, populateWeatherConditions);
+	});
+	forge.prefs.get("city", function(resource) {
+		if (resource) { // user has previously selected a city
+			var city = resource;
+		} else { // no previous selection
+			var city = "CA/San_Francisco";
+		}
+		$("#city_menu").val(city);
+		$("#city_menu").change();
+	}, function (error) {
+		forge.logging.error("failed when retrieving city preferences");
+		$("#city_menu").val("CA/San_Francisco"); // default;
+	});
 });
+
 
