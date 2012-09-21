bff8532d986a3e2151f5d4725447d9cee8dcfd30
Antoine van Gelder <antoine@trigger.io>
2012-09-21 12:03:40 +0200

    Cleanups

--- js/weather.js
+++ js/weather.js
@@ -5,15 +5,14 @@ window.forge.enableDebug();
 var weather = {
 	"current_observation": {
 		"display_location": {
-			"city": "San Francisco",
-			"state": "CA"
+			"full": "San Francisco, CA"
 		},
 		"observation_time":"Last Updated on September 20, 3:50 AM PDT",
 		"weather": "Mostly Cloudy",
 		"temp_f": 54.4,
 		"temp_c": 12.4,		
 		"relative_humidity":"89%",
-		"wind_string":"From the WNW at 4.0 MPH Gusting to 11.0 MPH",
+		"wind_string":"From the WNW at 4.0 MPH",
 		"icon_url":"http://icons-ak.wxug.com/i/c/k/nt_mostlycloudy.gif"
 	},
 	"forecast": {
@@ -29,8 +28,8 @@ var weather = {
 				  "period": 2,
 				  "high": { "fahrenheit": "70",	"celsius": "21"	},
 				  "low": { "fahrenheit": "54", "celsius": "12" },
-				  "conditions": "Partly Cloudy",
-				  "icon_url":"http://icons-ak.wxug.com/i/c/k/partlycloudy.gif" },
+				  "conditions": "Mostly Cloudy",
+				  "icon_url":"http://icons-ak.wxug.com/i/c/k/mostlycloudy.gif" },
 				{ "date": { "weekday_short": "Sat" },
 				  "period": 3,
 				  "high": { "fahrenheit": "70", "celsius": "21" },
@@ -42,7 +41,7 @@ var weather = {
 	}
 };
 
-forge.logging.log(weather);
+forge.logging.info(JSON.stringify(weather));
 
 
 /* - part 2 ------------------------------------------------------------- */
@@ -52,23 +51,23 @@ $(function () {
 	//getWeatherInfo("CA/San_Francisco", populateWeatherConditions);
 
 	// part 4
-	var cities = [ { name: "London", code: "UK/London" },
-				   { name: "San Francisco", code: "CA/San_Francisco" },
-				   { name: "Cape Town", code: "ZA/Cape_Town" },
-				   { name: "Barcelona", code: "ES/Barcelona" },
-				   { name: "Boston", code: "NY/Boston" },
-				   { name: "New York", code: "NY/New_York" },
-				   { name: "Washington DC", code: "DC/Washington" },
-				   { name: "Tampa", code: "FL/Tampa" },
-				   { name: "Houston", code: "AL/Houston" },
-				   { name: "Montreal", code: "CYUL" },
-				   { name: "Los Angeles", code: "CA/Los_Angeles" },
-				   { name: "Miami", code: "FL/Miami" },
-				   { name: "West Palm Beach", code: "FL/West_Palm_Beach" } ];
-	cities.forEach(function(city) {	// TODO use mustache
-		$("#city_menu").append("<option value='" + city.code + "'>" + 
-							   city.name +
-							   "</option>");
+	var cities = [ 
+		{ name: "London", code: "UK/London" },
+		{ name: "San Francisco", code: "CA/San_Francisco" },
+		{ name: "Cape Town", code: "ZA/Cape_Town" },
+		{ name: "Barcelona", code: "ES/Barcelona" },
+		{ name: "Boston", code: "NY/Boston" },
+		{ name: "New York", code: "NY/New_York" },
+		{ name: "Washington DC", code: "DC/Washington" },
+		{ name: "Tampa", code: "FL/Tampa" },
+		{ name: "Houston", code: "AL/Houston" },
+		{ name: "Montreal", code: "CYUL" },
+		{ name: "Los Angeles", code: "CA/Los_Angeles" },
+		{ name: "Miami", code: "FL/Miami" },
+		{ name: "West Palm Beach", code: "FL/West_Palm_Beach" } 
+	];
+	cities.forEach(function(city) {	
+		$("#city_menu").append("<option value='" + city.code + "'>" + city.name + "</option>");
 	});
 	$("#city_menu").change(function() {
 		var city = $("#city_menu option:selected").val();
@@ -91,55 +90,48 @@ $(function () {
 
 
 function populateWeatherConditions (weather) {
-    var tmpl, output;
+	var tmpl, output;
 
 	emptyContent(); // part 4
 
-    forge.logging.log("[populateWeatherConditions] beginning populating weather conditions");
+	forge.logging.log("[populateWeatherConditions] beginning populating weather conditions");
 
-    tmpl = $("#forecast_information_tmpl").html();
-    output = Mustache.to_html(tmpl, weather.current_observation);
-    $("#forecast_information").append(output);
-    forge.logging.log("[populateWeatherConditions] finished populating forecast information");
+	tmpl = $("#forecast_information_tmpl").html();
+	output = Mustache.to_html(tmpl, weather.current_observation);
+	$("#forecast_information").append(output);
+	forge.logging.log("[populateWeatherConditions] finished populating forecast information");
 
-    tmpl = $("#current_conditions_tmpl").html();
-    output = Mustache.to_html(tmpl, weather.current_observation);
-    $("#current_conditions").append(output);
-    forge.logging.log("[populateWeatherConditions] finished populating current conditions");
+	tmpl = $("#current_conditions_tmpl").html();
+	output = Mustache.to_html(tmpl, weather.current_observation);
+	$("#current_conditions").append(output);
+	forge.logging.log("[populateWeatherConditions] finished populating current conditions");
 
-    tmpl = $("#forecast_conditions_tmpl").html();
-    output = Mustache.to_html(tmpl, weather.forecast.simpleforecast);
-    $("#forecast_conditions table tr").append(output);
-    forge.logging.log("[populateWeatherConditions] finished populating forecast conditions");
+	tmpl = $("#forecast_conditions_tmpl").html();
+	output = Mustache.to_html(tmpl, weather.forecast.simpleforecast);
+	$("#forecast_conditions table tr").append(output);
+	forge.logging.log("[populateWeatherConditions] finished populating forecast conditions");
 
-    forge.logging.log("[populateWeatherConditions] finished populating weather conditions");
+	forge.logging.log("[populateWeatherConditions] finished populating weather conditions");
 };
 
 
 /* - part 3 ------------------------------------------------------------- */
 
-// curl http://api.wunderground.com/api/cef30204fb4bc178/conditions/forecast/q/CA/San_Francisco.json
-
 function getWeatherInfo(location, callback) {
 	var api_key = "cef30204fb4bc178";
-	var url = "http://api.wunderground.com/api/" + 
-			api_key +
-			"/conditions/forecast/q/" +	
-			location + 
-			".json"
-
 	forge.logging.info("[getWeatherInfo] getting weather for for " + location);
-    forge.request.ajax({
-        url: url,
+	forge.request.ajax({
+		url: "http://api.wunderground.com/api/" + api_key +
+				"/conditions/forecast/q/" +	location + ".json",
 		dataType: "json",
-        success: function (data) {
-            forge.logging.info("[getWeatherInfo] success");
+		success: function (data) {
+			forge.logging.info("[getWeatherInfo] success");
 			callback(data);
-        },
-        error: function (error) {
-            forge.logging.error("[getWeatherInfo] " + JSON.stringify(error));
-        }
-    });
+		},
+		error: function (error) {
+			forge.logging.error("[getWeatherInfo] " + JSON.stringify(error));
+		}
+	});
 };
 
 
@@ -148,9 +140,9 @@ function getWeatherInfo(location, callback) {
 function emptyContent() {
 	forge.logging.log("[emptyContent] removing old data");
 	$("#forecast_information").empty();
-    $("#current_conditions").empty();
-    $("#forecast_conditions table tr").empty();
+	$("#current_conditions").empty();
+	$("#forecast_conditions table tr").empty();
 	
-    forge.logging.log("[emptyContent] finished emptying content");
+	forge.logging.log("[emptyContent] finished emptying content");
 };
 
