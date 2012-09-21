847e0f05f1fd2680bd5bb4c44411e212dce42cc2
Antoine van Gelder <antoine@trigger.io>
2012-09-21 13:32:33 +0200

    Part 3

--- js/weather.js
+++ js/weather.js
@@ -1,45 +1,3 @@
-window.forge.enableDebug();
-
-var weather = {
-	"current_observation": {
-		"display_location": {
-			"full": "San Francisco, CA"
-		},
-		"observation_time":"Last Updated on September 20, 3:50 AM PDT",
-		"weather": "Mostly Cloudy",
-		"temp_f": 54.4,
-		"temp_c": 12.4,		
-		"relative_humidity":"89%",
-		"wind_string":"From the WNW at 4.0 MPH",
-		"icon_url":"http://icons-ak.wxug.com/i/c/k/nt_mostlycloudy.gif"
-	},
-	"forecast": {
-		"simpleforecast": {
-			"forecastday": [
-				{ "date": {	"weekday_short": "Thu" },
-				  "period": 1,
-				  "high": { "fahrenheit": "64", "celsius": "18" },
-				  "low": { "fahrenheit": "54", "celsius": "12" },
-				  "conditions": "Partly Cloudy",
-				  "icon_url":"http://icons-ak.wxug.com/i/c/k/partlycloudy.gif" },
-				{ "date": { "weekday_short": "Fri" },
-				  "period": 2,
-				  "high": { "fahrenheit": "70",	"celsius": "21"	},
-				  "low": { "fahrenheit": "54", "celsius": "12" },
-				  "conditions": "Mostly Cloudy",
-				  "icon_url":"http://icons-ak.wxug.com/i/c/k/mostlycloudy.gif" },
-				{ "date": { "weekday_short": "Sat" },
-				  "period": 3,
-				  "high": { "fahrenheit": "70", "celsius": "21" },
-				  "low": { "fahrenheit": "52", "celsius": "11" },
-				  "conditions": "Partly Cloudy",
-				  "icon_url":"http://icons-ak.wxug.com/i/c/k/partlycloudy.gif" }
-			]
-		}
-	}
-};
-
-
 function populateWeatherConditions (weather) {
 	var tmpl, output;
 
@@ -64,7 +22,25 @@ function populateWeatherConditions (weather) {
 };
 
 
+function getWeatherInfo(location, callback) {
+	var api_key = "YOUR_API_KEY";
+	forge.logging.info("[getWeatherInfo] getting weather for for " + location);
+	forge.request.ajax({
+		url: "http://api.wunderground.com/api/" + api_key +
+				"/conditions/forecast/q/" +	location + ".json",
+		dataType: "json",
+		success: function (data) {
+			forge.logging.info("[getWeatherInfo] success");
+			callback(data);
+		},
+		error: function (error) {
+			forge.logging.error("[getWeatherInfo] " + JSON.stringify(error));
+		}
+	});
+};
+
+
 $(function () {
-	populateWeatherConditions(weather);
+	getWeatherInfo("CA/San_Francisco", populateWeatherConditions);
 });
 
